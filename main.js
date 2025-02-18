a624bb5582467f6d7d93bde89811a645a84c4d91
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-02-18 16:45:16 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+
+/* const strongEl = document.getElementById("bold-text")
+console.log(strongEl);
+
+const portfolioEl = document.getElementById("portfolio")
+console.log(portfolioEl);
+
+const sectionEl = document.querySelector(".lorem")
+sectionEl.style.backgroundColor = "red"
+console.log(sectionEl); */
+
+
+
+        
+
+        const buttonEl = document.querySelector(".btn")
+        buttonEl.addEventListener("click", function(){
+           const elemento = document.querySelector(".lampOff")
+            elemento.src = "./img/yellow_lamp.png"
+            
+        })
+
+      /*   const elemento = document.querySelector("img")
+        function Accendi (){
+            const elemento = document.getElementById("lampOff")
+        } */
